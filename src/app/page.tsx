4121dda471505ad956e1699c04d634a5365e27b6
usmanarif460 <usmanarif460@gmail.com>
2025-04-07 04:00:24 +0500

    redirect user to next page after 3 seconds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 import Logo from "@/components/svgs/logo";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
   const router = useRouter();
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true); // Trigger fade-out effect
+      setTimeout(() => {
+        router.push("/create-account");
+      }, 500); // Wait for fade-out animation to complete before redirecting
+    }, 3000); // 5 seconds delay
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, [router]);
+
   return (
-    <div className="bg-[#00539b] h-screen flex flex-col items-center gap-2 justify-center">
+    <div
+      className={`bg-[#00539b] h-screen flex flex-col items-center gap-2 justify-center transition-opacity duration-500 ${
+        fadeOut ? "opacity-0" : "opacity-100"
+      }`}
+    >
       <div
         onClick={() => router.push("/create-account")}
         className="cursor-pointer"
